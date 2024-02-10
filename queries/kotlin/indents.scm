772bde435863651383275cdecd5a66c429661f32
YongJieYongJie <KhooYongJie@gmx.com>
2024-02-09 21:51:07 -0800

    [WIP] feat(kotlin): add indents queries

---
+++ queries/kotlin/indents.scm
@@ -0,0 +1,24 @@
+;; Based primarily on the indents.scm for c
+
+[
+  (primary_constructor)
+  (class_parameter)
+  (property_declaration)
+  (function_declaration)
+  (class_body)
+  (anonymous_initializer)
+  (call_suffix)
+  (navigation_expression)
+  (call_expression)
+] @indent.begin
+
+[ 
+  ")"
+  "]"
+] @indent.branch
+
+[
+  (line_comment)
+  (multiline_comment)
+] @indent.auto
+
